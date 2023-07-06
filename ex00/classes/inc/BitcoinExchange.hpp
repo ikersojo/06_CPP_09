c1672d81352f5b03adb03b438c25023a586112f0
Iker Sojo <ikersojo@hotmail.com>
2023-07-06 20:38:23 +0200

    update

---
+++ ex00/classes/inc/BitcoinExchange.hpp
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 16:00:48 by isojo-go          #+#    #+#             */
+/*   Updated: 2023/06/23 16:00:48 by isojo-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+	#define BITCOINEXCHANGE_HPP
+
+	#include <map>
+
+
+#endif // BITCOINEXCHANGE_HPPd
---
+++ ex00/inc/main.hpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 16:00:39 by isojo-go          #+#    #+#             */
+/*   Updated: 2023/06/23 16:02:30 by isojo-go         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MAIN_HPP
+	#define MAIN_HPP
+
+	#include <iostream>
+	#include <string>
+	#include "../classes/inc/BitcoinExchange.hpp"
+
+#endif // MAIN_HPP
