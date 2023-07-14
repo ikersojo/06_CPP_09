393a1f513a0a5b22b55dc00b2bd81392ef99feb8
Iker Sojo <ikersojo@hotmail.com>
2023-07-14 12:10:46 +0200

    update

--- ex01/classes/src/RPN.cpp
+++ ex01/classes/src/RPN.cpp
@@ -17,16 +17,20 @@ static bool	validChars(const char *str)
 	i = 0;
 	while (*(str + i))
 	{
-		if (!((*(str + i) >= '0' && *(str + i) <= '9') || (*(str + i) == '+' || *(str + i) == '-' || *(str + i) == '/' || *(str + i) == '*')))
+		if ((*(str + i) >= '0' && *(str + i) <= '9')
+		 || *(str + i) == ' '
+		 || *(str + i) == '+' || *(str + i) == '-'
+		 || *(str + i) == '/' || *(str + i) == '*')
+			i++;
+		else
 			return false;
-		i++;
 	}
 	return true;
 }
 
 RPN::RPN(const std::string& str)
 {
-	if (validChars(str.c_str()))
+	if (!validChars(str.c_str()))
 		throw std::invalid_argument("Error: invalid input!");
 	this->_str = str;
 	this->processStr();
@@ -65,5 +69,45 @@ std::string			RPN::getStr(void) const
 
 void	RPN::processStr(void)
 {
-	std::cout << "ALL OK" << std::endl;
+	std::cout << "STR OK: " << this->getStr() << std::endl; // DEBUG <-----------------
+
+	const char*	str = this->_str.c_str();
+	size_t		i = 0;
+	size_t		counter = 0;
+
+	i = 0;
+	while (*(str + i))
+	{
+		std::cout << *(str + i) << std::endl; // DEBUG <-----------------
+		if (*(str + i) >= '0' && *(str + i) <= '9')
+		{
+			this->_numberStack.push(*(str + i) - '0');
+			counter++;
+		}
+		else if (*(str + i) == ' ')
+			i++;
+		else
+		{
+			if (counter < 2)
+				throw std::invalid_argument("Error: invalid input!");
+			switch (*(str + i))
+			{
+				case '+':
+					std::cout << "adding... " << std::endl; // DEBUG <-----------------
+					break;
+				case '-':
+					std::cout << "substracting... " << std::endl; // DEBUG <-----------------
+					break;
+				case '/':
+					std::cout << "dividing... " << std::endl; // DEBUG <-----------------
+					break;
+				case '*':
+					std::cout << "multiplying... " << std::endl; // DEBUG <-----------------
+					break;
+				default:
+					break;
+			}
+		}
+		i++;
+	}
 }
